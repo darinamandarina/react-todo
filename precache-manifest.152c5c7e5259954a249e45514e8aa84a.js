89832df171b17a61ab458ca2d39e26555260aa77
darinamandarina <darinamandarina97@gmail.com>
2018-10-24 14:15:02 +0300

    add a minified project files

---
+++ precache-manifest.152c5c7e5259954a249e45514e8aa84a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b712d5f90a101864f02",
+    "url": "/static/js/main.2b712d5f.chunk.js"
+  },
+  {
+    "revision": "185192c561d33f51a9eb",
+    "url": "/static/js/1.185192c5.chunk.js"
+  },
+  {
+    "revision": "2b712d5f90a101864f02",
+    "url": "/static/css/main.714fb916.chunk.css"
+  },
+  {
+    "revision": "239a35696041dffe22f82befe8a2572a",
+    "url": "/index.html"
+  }
+];
